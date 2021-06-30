4c863b456965d52ea591a9887d22549c175dffa3
Galen Guyer <galen@galenguyer.com>
2021-06-30 15:26:31 -0700

    ca and config init commands

--- hancock.go
+++ hancock.go
@@ -1,16 +1,79 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
+	"os"
+	"strings"
 
 	"github.com/galenguyer/hancock/certs"
 	"github.com/galenguyer/hancock/config"
 	"github.com/galenguyer/hancock/keys"
 	"github.com/galenguyer/hancock/paths"
+	"github.com/urfave/cli/v2"
+	"gopkg.in/yaml.v3"
 )
 
-var (
-	defaultConf = &config.Config{
+const (
+	configPath = "./config.yaml"
+)
+
+func main() {
+	app := &cli.App{
+		Name: "hancock",
+		Commands: []*cli.Command{
+			{
+				Name:  "init",
+				Usage: "initialize the app",
+				Subcommands: []*cli.Command{
+					{
+						Name:  "config",
+						Usage: "create a new configuration file with all defaults",
+						Action: func(c *cli.Context) error {
+							return InitConfig()
+						},
+					},
+					{
+						Name:  "ca",
+						Usage: "initialize a new ca from the configuration file",
+						Action: func(c *cli.Context) error {
+							return InitCA()
+						},
+					},
+				},
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func InitConfig() error {
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		return newConfig()
+	} else {
+		fmt.Printf("Config file %s exists already - do you want to overwrite it? [yN]: ", configPath)
+		var prompt string
+		_, err = fmt.Scanln(&prompt)
+		if err != nil {
+			return err
+		}
+		if strings.ToLower(prompt) == "y" {
+			newConfig()
+		} else {
+			fmt.Println("not overwriting config and exiting")
+			os.Exit(0)
+		}
+	}
+	return nil
+}
+
+func newConfig() error {
+	defaultConf := &config.Config{
 		Key: struct {
 			Bits               int    "yaml:\"bits\""
 			Lifetime           int    "yaml:\"lifetime\""
@@ -36,31 +99,58 @@ var (
 			BaseDir: "./.ca",
 		},
 	}
-)
-
-func main() {
-	conf := defaultConf
-
-	err := paths.CreateDirectories(*conf)
+	yamlBytes, err := yaml.Marshal(defaultConf)
 	if err != nil {
-		log.Fatalln(err)
+		return err
+	}
+	err = ioutil.WriteFile(configPath, yamlBytes, 0644)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("created new config file at %s\n", configPath)
+	return nil
+
+}
+
+func readConfig() (*config.Config, error) {
+	yamlFile, err := ioutil.ReadFile(configPath)
+	if err != nil {
+		return nil, err
+	}
+	var conf config.Config
+	err = yaml.Unmarshal(yamlFile, conf)
+	if err != nil {
+		return nil, err
+	}
+	return &conf, nil
+}
+
+func InitCA() error {
+	conf, err := readConfig()
+	if err != nil {
+		return err
+	}
+	err = paths.CreateDirectories(*conf)
+	if err != nil {
+		return err
 	}
 
 	key, err := keys.GenerateRootRsaKey(*conf)
 	if err != nil {
-		log.Fatalln(err)
+		return err
 	}
 	err = keys.SaveRootRsaKey(*key, *conf)
 	if err != nil {
-		log.Fatalln(err)
+		return err
 	}
 
 	caCertBytes, err := certs.GenerateRootCACert(*key, *conf)
 	if err != nil {
-		log.Fatalln(err)
+		return err
 	}
 	err = certs.SaveRootCACert(caCertBytes, *conf)
 	if err != nil {
-		log.Fatalln(err)
+		return err
 	}
+	return nil
 }
