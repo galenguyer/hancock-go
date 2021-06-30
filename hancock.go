3157904516435ac3eebc1f4d923563ecc912449c
Galen Guyer <galen@galenguyer.com>
2021-06-30 13:22:45 -0700

    add basic config and rsa key generation and saving

--- hancock.go
+++ hancock.go
@@ -2,10 +2,46 @@ package main
 
 import (
 	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
 	"math/big"
 )
 
+type Config struct {
+	Key struct {
+		Bits int `yaml:"bits"`
+	} `yaml:"key"`
+}
+
+var (
+	defaultConf = &Config{
+		Key: struct {
+			Bits int "yaml:\"bits\""
+		}{
+			Bits: 4096,
+		},
+	}
+)
+
 func main() {
+	generateRootRsaKey(*defaultConf)
+}
+
+func generateRootRsaKey(conf Config) error {
+	rootRsaKey, err := rsa.GenerateKey(rand.Reader, conf.Key.Bits)
+	if err != nil {
+		return err
+	}
+	fmt.Print(saveRootRsaKey(*rootRsaKey))
+	return nil
+}
+
+func saveRootRsaKey(key rsa.PrivateKey) string {
+	keyPem := &pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(&key)}
+	bytes := pem.EncodeToMemory(keyPem)
+	return string(bytes)
 }
 
 func getSerial() (*big.Int, error) {
